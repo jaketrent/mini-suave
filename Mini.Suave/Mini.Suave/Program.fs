5991a666b481cc48abfcb54f428132f51d6afd70
jaketrent <trent.jake@gmail.com>
2016-11-17 13:07:48 -0700

    initial commit, single response

---
+++ Mini.Suave/Mini.Suave/Program.fs
@@ -0,0 +1,17 @@
+﻿module MiniSuave
+
+open Suave.Http
+open Suave.Console
+open Suave.Successful
+
+[<EntryPoint>]
+let main argv = 
+    let request = { Route = ""; Type = Suave.Http.GET }
+    let response = { Content = ""; StatusCode = 200 }
+    let context = { Request = request; Response = response}
+    
+    execute context (OK "Hello Jake")
+    
+    
+    0
+
